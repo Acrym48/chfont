114288c77db9da9af439c21016eb911daf0b7f86
Acrym48 <zona228_nb@mail.ru>
2024-05-05 02:47:40 +0500

    beta

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chfont
+The script swaps the font.ttf and font2.ttf files in the folder .termux
+> This script is designed for Termux
---
+++ installer.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Acrym48/filechfont.git
+cp filechfont/chfont ~/../usr/bin/chfont
+cp filechfont/font.ttf ~/.termux/font.ttf
+cp filechfont/font2.ttf ~/.termux/font2.ttf
+rm -Rf filechfont
+echo "chfont installed"
